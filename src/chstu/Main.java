ce3ef2c005a9e2986d9dd22905f8e9a2b183396c
VitaliiLynnyk <linnik2101@gmail.com>
2017-05-13 20:49:01 +0300

    push top left and center panel

--- src/chstu/Main.java
+++ src/chstu/Main.java
@@ -6,7 +6,6 @@ import chstu.Interface.GUI;
 public class Main {
 
     public static void main(String[] args) {
-        Calendars calendars = new Calendars();
         GUI form = new GUI();
         form.makeForm();
     }
