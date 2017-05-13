6379423d4e3d8ef09d10983a47db06c646fb5afe
ARK <ar-krav@meta.ua>
2017-05-13 23:28:19 +0300

    correct DB information

--- src/chstu/timetable/Bot.java
+++ src/chstu/timetable/Bot.java
@@ -2,6 +2,8 @@ package chstu.timetable;
 
 import chstu.db.DBAdapter;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Bot {
@@ -12,4 +14,17 @@ public class Bot {
 
     Date currentDate;
     DBAdapter dbAdapter;
+
+    private String getDate(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(currentDate).toString();
+    }
+
+    public void checkUserActivity(){
+        ArrayList<Integer> subjectForPass = dbAdapter.getSubjectsForPass(getDate());
+
+        if (subjectForPass.size() == 0) {
+            System.out.println("No subject for pass today!");
+        }
+    }
 }
