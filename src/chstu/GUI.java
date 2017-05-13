899b30b9ea712f6acfe5147575ccfa61cfd05644
VitaliiLynnyk <linnik2101@gmail.com>
2017-05-13 14:06:04 +0300

    GUI(left)

---
+++ src/chstu/GUI.java
@@ -0,0 +1,65 @@
+package chstu;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by linni on 5/13/2017.
+ */
+public class GUI {
+
+    public void makeForm(){
+        JFrame projectFrame = new JFrame();
+        projectFrame.setSize(1200,700);
+        projectFrame.setTitle("frame");
+        projectFrame.setResizable(false);
+
+        projectFrame.setDefaultCloseOperation(projectFrame.EXIT_ON_CLOSE);
+        projectFrame.setLocationRelativeTo(null);
+        projectFrame.setLayout(null);
+        projectFrame.setBackground(Color.white);
+
+        String[] firstMenuItem = {
+                "Lecce_Pen",
+                "Parker_Pen",
+                "AURORA"
+        };
+        JMenuBar firstMenu = new JMenuBar();
+        firstMenu.setBounds(0,0,950,40);
+        projectFrame.add(firstMenu);
+
+        JMenuItem item = new JMenuItem("firstMenuItem");
+        item.setBounds(0,5,100,40);
+        firstMenu.add(item);
+
+        JButton btSearch = new JButton("search");
+        btSearch.setBounds(1100,5,100,30);
+        projectFrame.add(btSearch);
+
+        JTextField textSearch = new JTextField();
+        textSearch.setBounds(950,5,150,30);
+        projectFrame.add(textSearch);
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBounds(0,40,250,630);
+        leftPanel.setBackground(Color.BLACK);
+        projectFrame.add(leftPanel);
+
+
+        JButton[] btn = new JButton[20];
+        for (int i = 0; i < 20; i++) {
+            btn[i] = new JButton(i +"");
+            btn[i].setPreferredSize(new Dimension(100,80));
+            leftPanel.setLayout(new GridLayout(0,1));
+
+            leftPanel.add(btn[i]);
+        }
+
+        JScrollPane jScrollPaneLeftPanel  = new JScrollPane(leftPanel,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPaneLeftPanel.setBounds(0,40,250,630);
+        projectFrame.add(jScrollPaneLeftPanel);
+
+        projectFrame.setVisible(true);
+    }
+}
--- src/chstu/Main.java
+++ src/chstu/Main.java
@@ -3,7 +3,7 @@ package chstu;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //yo yo
+        GUI form = new GUI();
+        form.makeForm();
     }
 }
