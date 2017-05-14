ea26fd1a98373bb0659e01e0181d913f3ac878ab
ARK <ar-krav@meta.ua>
2017-05-14 04:49:01 +0300

    Separate method for processing date to another Class

--- src/chstu/timetable/Bot.java
+++ src/chstu/timetable/Bot.java
@@ -14,31 +14,15 @@ public class Bot {
 
     Date currentDate;
     DBAdapter dataBase;
+    DateUtil dateUtil = new DateUtil();
+
     ArrayList<Integer> subjectsForPassToday;
     ArrayList<Integer> numberLessonsOfSubjectForPass;
     int inProcess = 0, passedLab = 1, debt = 2;
 
-    private String getCurrentDate() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        return dateFormat.format(currentDate);
-    }
-
-    private Date getCurrentTime(){
-        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
-        Date currentTimeDate = null;
-
-        try {
-            currentTimeDate = dateFormat.parse(dateFormat.format(currentDate));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return currentTimeDate;
-    }
 
     public void checkUserDutyForToday() {
-        subjectsForPassToday = dataBase.getSubjectsForPass(getCurrentDate());
+        subjectsForPassToday = dataBase.getSubjectsForPass(dateUtil.getCurrentDate());
         numberLessonsOfSubjectForPass = new ArrayList<>();
 
         if (subjectsForPassToday.size() == 0) {
@@ -47,7 +31,7 @@ public class Bot {
         }
 
         for (int i = 0; i < subjectsForPassToday.size(); i++) {
-             ArrayList<Integer> numbersOfLessonsForOneSubject = dataBase.getNumberOfLessonsForPassedSubjects(subjectsForPassToday.get(i), getCurrentDate());
+             ArrayList<Integer> numbersOfLessonsForOneSubject = dataBase.getNumberOfLessonsForPassedSubjects(subjectsForPassToday.get(i), dateUtil.getCurrentDate());
              for (int j = 0; i < numbersOfLessonsForOneSubject.size(); j++){
                  numberLessonsOfSubjectForPass.add(numbersOfLessonsForOneSubject.get(j));
              }
@@ -57,14 +41,26 @@ public class Bot {
     private void checkLabsStatys(){
         ArrayList<Date> endOfLessons = dataBase.getEndOfLessons();
         for (int i = 0; i < numberLessonsOfSubjectForPass.size(); i++){
-            int subject = dataBase.getSubjectByLessonNuberAtDay(numberLessonsOfSubjectForPass.get(i),getCurrentDate());
-            if (getCurrentTime().getTime() >= endOfLessons.get(numberLessonsOfSubjectForPass.get(i)-1).getTime()){
-                if(dataBase.getlabStatus(subject,getCurrentDate()) == inProcess) dataBase.setLabStatus(subject,getCurrentDate(),debt);
+            int subject = dataBase.getSubjectByLessonNuberAtDay(numberLessonsOfSubjectForPass.get(i),dateUtil.getCurrentDate());
+            if (dateUtil.getCurrentTime().getTime() >= endOfLessons.get(numberLessonsOfSubjectForPass.get(i)-1).getTime()){
+                if(dataBase.getlabStatus(subject,dateUtil.getCurrentDate()) == inProcess) dataBase.setLabStatus(subject,dateUtil.getCurrentDate(),debt);
             }
         }
     }
 
+    private boolean isMoreLessonsToday(){
+        if(dateUtil.getCurrentTime().getTime() >= dataBase.getEndOfLessons().get(dataBase.getNumberLessonsInDay(dateUtil.getCurrentDate())-1).getTime()) return false;
+        else return true;
+    }
+
     /*private long getTimeToNextLesson(){
-        cur
+        if(isMoreLessonsToday()) {
+            for (int i = 0; i < dataBase.getEndOfLessons().size(); i++){
+                if (dateUtil.getCurrentTime().getTime() < dataBase.getEndOfLessons().get(i).getTime()){
+                    //return dataBase.getEndOfLessons().get(i).getTime() - dateUtil.getCurrentTime();
+                }
+            }
+        }
+        return  4444;
     }*/
 }
